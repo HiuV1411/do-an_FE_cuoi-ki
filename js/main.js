a35ce74bffbecfc3406ed2f175d43092014bbea8
Nguyen Minh <nguyenminh01062005@gmail.com>
2024-10-28 07:03:06 +0700

    28/10/2024 - Nguyen Minh

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
+let slider = document.querySelector('.container2 .slider');
+let slides = document.querySelectorAll('.container2 .slider .slide');
+let dots = document.querySelectorAll('.container2 .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');;
 
-//JavaScript cho điều hướng các ảnh
-var manualNav = function (manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
+let active = 0;
+let lengthSlide = slides.length - 1;
 
-        btns.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-    });
-
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
+prev.onclick = function()
+{
+    if(active - 1 < 0)
+    {
+        active = lengthSlide;
+    }
+    else
+    {
+        active -= 1;
+    }
+    reloadSlider();
 }
 
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-    });
-});
+next.onclick = function() 
+{
+    if (active + 1 > lengthSlide)
+    {
+        active = 0;
+    }
+    else
+    {
+        active += 1;
+    }
+    reloadSlider();
+}
+
+function reloadSlider()
+{
+    let checkLeft = slides[active].offsetLeft;
+    slider.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.container2 .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+})
+

65402f2e7adc927e06555fb32e444e78947bc2fd
Nguyen Minh <nguyenminh01062005@gmail.com>
2024-10-29 18:29:24 +0700

    29/10/2024 - Nguyen Minh

--- js/main.js
+++ js/main.js
@@ -1,52 +1,85 @@
-let slider = document.querySelector('.container2 .slider');
-let slides = document.querySelectorAll('.container2 .slider .slide');
-let dots = document.querySelectorAll('.container2 .dots li');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');;
+const slider = document.querySelector(".slider");
+const nextbtn = document.querySelector(".fa-chevron-right");
+const prevbtn = document.querySelector(".fa-chevron-left");
+const slides = document.querySelectorAll(".slide");
+const buttons = document.querySelectorAll(".btn");
+const numberofslides = slides.length;
 
-let active = 0;
-let lengthSlide = slides.length - 1;
+var slidenumber = 0;
+var playslider;
 
-prev.onclick = function()
-{
-    if(active - 1 < 0)
-    {
-        active = lengthSlide;
-    }
-    else
-    {
-        active -= 1;
-    }
-    reloadSlider();
+// Tự động chuyển slides
+var repeat = () => {
+    playslider = setInterval(function () {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+        buttons.forEach((button) => {
+            button.classList.remove("active");
+        });
+
+        slidenumber++;
+
+        if (slidenumber > (numberofslides - 1)) {
+            slidenumber = 0;
+        }
+        slides[slidenumber].classList.add("active");
+        buttons[slidenumber].classList.add("active");
+    }, 60000);
 }
+repeat();
 
-next.onclick = function() 
-{
-    if (active + 1 > lengthSlide)
-    {
-        active = 0;
+// Slide tiếp theo khi nhấn vào các nút ở bên dưới
+buttons.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+
+        slidenumber = i;
+
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+
+        buttons.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+
+        slides[slidenumber].classList.add('active');
+        buttons[slidenumber].classList.add('active');
+    });
+});
+
+// Slide tiếp theo khi nhấn chevron-right
+nextbtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+
+    slidenumber++;
+
+    if (slidenumber > (numberofslides - 1)) {
+        slidenumber = 0;
     }
-    else
-    {
-        active += 1;
+    slides[slidenumber].classList.add("active");
+    buttons[slidenumber].classList.add("active");
+});
+
+// Slide tiếp theo khi nhấn chevron-left
+prevbtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+
+    slidenumber--;
+
+    if (slidenumber < 0) {
+        slidenumber = numberofslides - 1;
     }
-    reloadSlider();
-}
-
-function reloadSlider()
-{
-    let checkLeft = slides[active].offsetLeft;
-    slider.style.left = -checkLeft + 'px';
-
-    let lastActiveDot = document.querySelector('.container2 .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
-}
-
-dots.forEach((li, key) => {
-    li.addEventListener('click', function(){
-        active = key;
-        reloadSlider();
-    })
-})
-
+    slides[slidenumber].classList.add("active");
+    buttons[slidenumber].classList.add("active");
+});

0c9d07a67e8f59e66cd0b371d01417e796cc1c00
Vohieu <hieuvo1411@gmail.com>
2024-11-06 23:45:44 +0700

    6/11/2024_DucHieu

--- js/main.js
+++ js/main.js
@@ -10,96 +10,103 @@ var playslider;
 
 // Tự động chuyển slides
 var repeat = () => {
-    playslider = setInterval(function () {
-        slides.forEach((slide) => {
-            slide.classList.remove("active");
-        });
-        buttons.forEach((button) => {
-            button.classList.remove("active");
-        });
-
-        slidenumber++;
-
-        if (slidenumber > (numberofslides - 1)) {
-            slidenumber = 0;
-        }
-        slides[slidenumber].classList.add("active");
-        buttons[slidenumber].classList.add("active");
-    }, 60000);
-}
-repeat();
-
-// Slide tiếp theo khi nhấn vào các nút ở bên dưới
-buttons.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-
-        slidenumber = i;
-
-        slides.forEach((slide) => {
-            slide.classList.remove('active');
-        });
-
-        buttons.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-
-        slides[slidenumber].classList.add('active');
-        buttons[slidenumber].classList.add('active');
-    });
-});
-
-// Slide tiếp theo khi nhấn chevron-right
-nextbtn.addEventListener("click", () => {
+  playslider = setInterval(function () {
     slides.forEach((slide) => {
-        slide.classList.remove("active");
+      slide.classList.remove("active");
     });
     buttons.forEach((button) => {
-        button.classList.remove("active");
+      button.classList.remove("active");
     });
 
     slidenumber++;
 
-    if (slidenumber > (numberofslides - 1)) {
-        slidenumber = 0;
+    if (slidenumber > numberofslides - 1) {
+      slidenumber = 0;
     }
     slides[slidenumber].classList.add("active");
     buttons[slidenumber].classList.add("active");
+  }, 60000);
+};
+repeat();
+
+// Slide tiếp theo khi nhấn vào các nút ở bên dưới
+buttons.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    slidenumber = i;
+
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    slides[slidenumber].classList.add("active");
+    buttons[slidenumber].classList.add("active");
+  });
+});
+
+// Slide tiếp theo khi nhấn chevron-right
+nextbtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  slidenumber++;
+
+  if (slidenumber > numberofslides - 1) {
+    slidenumber = 0;
+  }
+  slides[slidenumber].classList.add("active");
+  buttons[slidenumber].classList.add("active");
 });
 
 // Slide tiếp theo khi nhấn chevron-left
 prevbtn.addEventListener("click", () => {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-    buttons.forEach((button) => {
-        button.classList.remove("active");
-    });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    slidenumber--;
+  slidenumber--;
 
-    if (slidenumber < 0) {
-        slidenumber = numberofslides - 1;
-    }
-    slides[slidenumber].classList.add("active");
-    buttons[slidenumber].classList.add("active");
+  if (slidenumber < 0) {
+    slidenumber = numberofslides - 1;
+  }
+  slides[slidenumber].classList.add("active");
+  buttons[slidenumber].classList.add("active");
 });
 
 // Khi cuộn xuống thì thanh top-header sẽ nảy từ thanh công cụ của trình duyệt ra
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('.list-item');
-    const topHeader = document.querySelector('.top-header');
-    if (window.scrollY > 50) {
-        if (!header.classList.contains('scrolled')) {
-            header.classList.add('abounce');
-            setTimeout(() => {
-                header.classList.remove('abounce');
-                header.classList.add('scrolled');
-                topHeader.style.height = '60px';
-            }, 200);
-        }
-    } else {
-        header.classList.remove('scrolled');
-        topHeader.style.height = '90px';
-        header.style.backgroundColor = '';
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".list-item");
+  const topHeader = document.querySelector(".top-header");
+  if (window.scrollY > 50) {
+    if (!header.classList.contains("scrolled")) {
+      header.classList.add("abounce");
+      setTimeout(() => {
+        header.classList.remove("abounce");
+        header.classList.add("scrolled");
+        topHeader.style.height = "60px";
+      }, 200);
     }
+  } else {
+    header.classList.remove("scrolled");
+    topHeader.style.height = "90px";
+    header.style.backgroundColor = "";
+  }
+});
+
+// Khi bấm vào icon cart sẽ hiện ra cart-tab
+let iconCart = document.querySelector("icon-cart");
+let body = document.querySelector("body");
+
+iconCart.addEventListener("click", () => {
+  body.classList.toggle("showcart");
 });
